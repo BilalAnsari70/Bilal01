b1eec443789a6032e769721683525b6dc9dc7b9e
kus1526 <129712066+kus1526@users.noreply.github.com>
2023-04-05 09:08:07 +0530

    This is my first code

---
+++ src/Bilal.java
@@ -0,0 +1,40 @@
+class Test
+{
+	public void show()
+	{
+		System.out.println("Ansari");
+	}
+}
+
+class Display
+{
+	public void show()
+	{
+		System.out.println("Blockchain");
+	}
+}
+
+class Input
+{
+	public void show()
+	{
+		System.out.println("CSO");
+	}
+}
+
+public class Bilal {
+
+	public static void main(String[] args) {
+		Test Ansari=new Test();
+		Ansari.show();
+		
+		Display Blockchain=new Display();
+		Blockchain.show();
+		
+		Input CSO=new Input();
+		CSO.show();
+		
+		System.out.println("Bilal");
+	
+	}
+}
